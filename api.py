18862cf319ec92ab958562418232ba3d2c70f9ce
Timothy Samraj <tsam0005@student.monash.edu>
2020-11-01 13:33:11 +1100

    API can now return results in natural language

--- api.py
+++ api.py
@@ -12,5 +12,21 @@ def members():
     if flask.request.headers.get("Authorization") == auth.HEADER_AUTH:
         with open("voice_lst", "r") as f:
             data = json.load(f)
-        return flask.jsonify(data), 200
+        if flask.request.headers.get("Response-Type") == "natural":
+            num_channels = len({k: v for (k, v) in data.items() if len(v) > 0})
+            num_members = sum(len(sub) for sub in data.values())
+            # How far is too far?
+            member_lst = list(
+                map(
+                    lambda x: x.get("name"),
+                    [
+                        item for sublist in [sub for sub in data.values()]
+                        for item in sublist
+                    ],
+                ))
+
+            return "{} people in {} channels: {}".format(
+                num_members, num_channels, ", ".join(member_lst)), 200
+        else:
+            return flask.jsonify(data), 200
     return "<h1>NO</h1>", 403
