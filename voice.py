73694c943b630865e22bd0eb3da8c41a7784a75a
Timothy Samraj <tsam0005@student.monash.edu>
2020-11-21 21:44:39 +1100

    Add counter of live members to API

--- voice.py
+++ voice.py
@@ -13,6 +13,7 @@ attached_guild: discord.Guild = None
 async def on_voice_state_update(member=None, before=None, after=None):
     assert attached_guild is not None
     member_count = 0
+    live_count = 0
     occupied_channel_count = 0
     voice_data = {"channels": {}}
     for channel in attached_guild.voice_channels:
@@ -21,6 +22,7 @@ async def on_voice_state_update(member=None, before=None, after=None):
         for member in channel.members:
             nonempty = True
             member_count += 1
+            live_count += 1 if member.voice.self_stream else 0
             channel_member_lst.append(
                 {
                     "name": member.name,
@@ -31,11 +33,11 @@ async def on_voice_state_update(member=None, before=None, after=None):
                     "server_mute": member.voice.mute,
                 }
             )
-        if nonempty:
-            occupied_channel_count += 1
-        voice_data["occupied_channels"] = occupied_channel_count
-        voice_data["member_count"] = member_count
+        occupied_channel_count += 1 if nonempty else 0
         voice_data["channels"][str(channel)] = channel_member_lst
+    voice_data["occupied_channels"] = occupied_channel_count
+    voice_data["member_count"] = member_count
+    voice_data["live_count"] = live_count
     with open("voice_lst", "w") as f:
         json.dump(voice_data, f)
     print("[" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "] updated")
